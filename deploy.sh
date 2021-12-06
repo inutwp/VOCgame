25558e32dbf84e083e84f0f419ba96a46924e1eb
Tegar Wisnu Pambudi <sukandaru.japan@gmail.com>
2021-12-06 12:03:31 +0700

    change image to alpine:3.15

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ echo "Down Service...."
 docker-compose down --remove-orphans
 docker system prune -a -f
 echo "Build and Up Service...."
-docker-compose up -d --build --remove-orphans
+docker-compose build --no-cache --pull
+docker-compose up -d --remove-orphans
 SUCCESS_UP_SERVICE=$?
 if [[ ${SUCCESS_UP_SERVICE} -eq 0 ]]; then
     echo "Clear Redundant"   
