c98ee61d5b0e96aac3f0467bc1527638ed736285
Tegar Wisnu Pambudi <inutwp@gmail.com>
2021-12-09 09:16:33 +0700

    separate nginx dockerfile

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ clear
 echo "Down Service...."
 docker-compose down --remove-orphans
 docker system prune -a -f
+echo ""
+
 echo "Build and Up Service...."
 docker-compose build --no-cache --pull
 docker-compose up -d --remove-orphans
